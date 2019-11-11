c54af463d867175ef785d78618f3cb21c0f5ca2e
vai na web <vainawebcpx@gmail.com>
2019-11-11 16:39:21 -0300

    brenno

---
+++ main.js
@@ -0,0 +1,8 @@
+let lupa = document.getElementById('lupa')
+console.log('lupa')
+let busca = document.getElementById('busca')
+console.log('busca')
+
+lupa.addEventListener('click', function(){
+	busca.style.display = 'flex'
+})
